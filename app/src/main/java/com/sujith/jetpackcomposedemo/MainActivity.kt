41dfebe50e5ffa23123feb5965f6df08539b3a95
sujithmanjavana <sujithmanjavana@gmail.com>
2023-11-18 12:21:52 +0530

    fc

---
+++ app/src/main/java/com/sujith/jetpackcomposedemo/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.sujith.jetpackcomposedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sujith.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+
+                Text(
+                    text = "Hello, World!",
+                    modifier = Modifier.requiredWidth(150.dp).background(Color.Yellow)
+                )
+                Text(
+                    text = "Jetpack Compose",
+                    modifier = Modifier.requiredWidth(40.dp).background(Color.Red),
+                    maxLines = 1
+                )
+
+            }
+        }
+    }
+}
+
