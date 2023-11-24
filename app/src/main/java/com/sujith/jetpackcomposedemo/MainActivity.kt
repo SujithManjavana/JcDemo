06d1b840416b7957e0c17933d217dcec19a7756f
sujithmanjavana <sujithmanjavana@gmail.com>
2023-11-24 21:16:46 +0530

    add basic card

--- app/src/main/java/com/sujith/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/sujith/jetpackcomposedemo/MainActivity.kt
@@ -3,15 +3,23 @@ package com.sujith.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,40 +27,57 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sujith.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Cyan)
-                    .fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.Top
-            ) {
-
-
-                Text(
-                    text = "Hello, World!",
-                    modifier = Modifier
-                        .requiredWidth(150.dp)
-                        .background(Color.Yellow)
-                )
-                Text(
-                    text = "Jetpack Compose",
-                    modifier = Modifier
-                        .requiredWidth(40.dp)
-                        .background(Color.Red),
-                    maxLines = 1
-                )
-
-            }
+            val painter = painterResource(id = R.drawable.bird)
+            val contentDescription = "A snow bird"
+            val title = "A snow bird"
+            ImageCard(painter = painter, contentDescription = contentDescription, title = title)
         }
     }
 }
 
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    )
+    {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentScale = ContentScale.Crop,
+                contentDescription = contentDescription
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+
+        }
+    }
+}
