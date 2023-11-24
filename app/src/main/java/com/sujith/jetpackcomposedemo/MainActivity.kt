ae2393bf4f38ef582986a44e010e6f49bea2c8af
sujithmanjavana <sujithmanjavana@gmail.com>
2023-11-24 21:48:19 +0530

    finish card

--- app/src/main/java/com/sujith/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/sujith/jetpackcomposedemo/MainActivity.kt
@@ -5,36 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.sujith.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +33,14 @@ class MainActivity : ComponentActivity() {
             val painter = painterResource(id = R.drawable.bird)
             val contentDescription = "A snow bird"
             val title = "A snow bird"
-            ImageCard(painter = painter, contentDescription = contentDescription, title = title)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(12.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = contentDescription, title = title)
+            }
+
         }
     }
 }
@@ -58,7 +54,8 @@ fun ImageCard(
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth(),
         shape = RoundedCornerShape(15.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
     )
@@ -67,7 +64,18 @@ fun ImageCard(
             Image(
                 painter = painter,
                 contentScale = ContentScale.Crop,
-                contentDescription = contentDescription
+                contentDescription = contentDescription,
+                modifier = Modifier.fillMaxSize()//from stackoverflow
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 250f
+                        )
+                    )
             )
             Box(
                 modifier = Modifier
