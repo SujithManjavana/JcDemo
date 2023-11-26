b229096ae7baf4683483fed8672287918bf669b6
sujithmanjavana <sujithmanjavana@gmail.com>
2023-11-26 18:40:09 +0530

    complete text styling

--- app/src/main/java/com/sujith/jetpackcomposedemo/TextStyleActivity.kt
+++ app/src/main/java/com/sujith/jetpackcomposedemo/TextStyleActivity.kt
@@ -13,11 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,7 +54,16 @@ fun MainView() {
     ) {
 
         Text(
-            text = "Jetpack compose",
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)) {
+                    append("H")
+                }
+                append("ello, ")
+                withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)) {
+                    append("W")
+                }
+                append("orld!")
+            },
             color = Color.White,
             fontSize = 30.sp,
             fontFamily = fontFamily,
