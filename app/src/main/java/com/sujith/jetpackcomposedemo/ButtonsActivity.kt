0b71cb4cf674c4f0969c779098275310482ef2d3
sujithmanjavana <sujithmanjavana@gmail.com>
2023-12-02 14:37:05 +0530

    finish button activity

--- app/src/main/java/com/sujith/jetpackcomposedemo/ButtonsActivity.kt
+++ app/src/main/java/com/sujith/jetpackcomposedemo/ButtonsActivity.kt
@@ -2,36 +2,75 @@ package com.sujith.jetpackcomposedemo
 
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.*
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class ButtonsActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            val snackbarHostState = remember { SnackbarHostState() }
+            val snackbarHostState = remember { SnackbarHostState() }
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
 
             // Create a coroutineScope for the animation
             val scope = rememberCoroutineScope()
-            Scaffold {
-                Column (
-                    modifier = Modifier.padding(it),
+            Scaffold(
+                snackbarHost = { SnackbarHost(snackbarHostState) },
+                modifier = Modifier.fillMaxSize()
+            ) { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(horizontal = 30.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
-
-                ){
-
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true,
+                        value = textFieldState,
+                        label = { Text(text = "Enter your name") },
+                        onValueChange = { newValue ->
+                            textFieldState = newValue
+                        })
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = textFieldState,
+                        label = { Text(text = "Some label") },
+                        onValueChange = {})
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(message = "Hello $textFieldState")
+                        }
+                    }) {
+                        Text(text = "Greet me!")
+                    }
                 }
             }
         }
