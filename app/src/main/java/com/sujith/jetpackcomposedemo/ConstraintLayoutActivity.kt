7b07a9e402daceef1bcf11d83b02381f13a3d82f
sujithmanjavana <sujithmanjavana@gmail.com>
2023-12-08 14:46:59 +0530

    finish constraints layout

--- app/src/main/java/com/sujith/jetpackcomposedemo/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/sujith/jetpackcomposedemo/ConstraintLayoutActivity.kt
@@ -4,15 +4,55 @@ package com.sujith.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class ConstraintLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val constrains = ConstraintSet{
-                val greenBox=createRefFor("greenBox")
-                val redBox=createRefFor("redBox")
+            val constrains = ConstraintSet {
+                val greenBox = createRefFor("green_box")
+                val redBox = createRefFor("red_box")
+
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Spread)
+            }
+
+            ConstraintLayout(constrains, modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .layoutId("green_box")
+                )
+                Box(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .layoutId("red_box")
+                )
             }
         }
     }
